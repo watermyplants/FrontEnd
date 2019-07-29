70cd83eafe6a7f0b1d33ef3505e91c215acccd8e
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-07-29 16:15:17 -0400

    so far

--- water_my_plants/src/App.js
+++ water_my_plants/src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Login from './components/Login'
 
 import "./App.css";
 
@@ -6,6 +7,7 @@ function App() {
   return (
     <div className="App">
       <h1>Water My Plants</h1>
+      <Login />
     </div>
   );
 }
---
+++ water_my_plants/src/components/login.js
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+export default function Login() {
+
+
+  const [signIn, setSignIn] = useState({
+    email: "",
+    password: "",
+  });
+
+
+
+  function submitHandler(event){
+    event.preventDefault();
+    console.log("sign in", signIn);
+    setSignIn(signIn)////adding member to the form ?
+
+  };
+
+  function changeHandler(event) {
+    console.log(signIn);
+    setSignIn({ ...signIn, [event.target.name]: event.target.value });
+
+  };
+
+  return (
+    <div className='login-card'>
+      <form onSubmit={submitHandler}>
+        <legend className='sign-in-header'>Sign in</legend>
+        <br />
+          <div className='name'>
+            <label>
+              Email Address
+              <br />
+              <input className='email-input'
+                type="text"
+                name="email"
+                value={signIn.email}
+                placeholder=""
+                onChange={changeHandler}
+              />
+              <hr />
+            </label>
+          </div>
+          <div className="password">
+            <label>
+              Password
+              <br />
+              <input className='password-input'
+                type="password"
+                name="password"
+                value={signIn.password}
+                placeholder=""
+                onChange={changeHandler}
+              />
+              <hr />
+            </label>
+            <p className='forgot'>Forgot your password?</p>
+            <br />
+          </div>
+          <button className='sign-in-button' type="submit" >Sign In</button>
+      </form>
+      <div className='bottom-signin'>
+        <p className='need'>Need an account?</p>
+        <h5 className='sign-up-btn'>Sign up</h5>
+      </div>
+    </div>
+  )
+} 
