f5856040ffed9f6507553dd5a90fcddfd35a396f
Luis De Anda <luisdeanda916@outlook.com>
2019-07-29 13:42:33 -0700

    Form to add a new plant created

---
+++ water_my_plants/src/components/NewPlant.js
@@ -0,0 +1,52 @@
+import React, {useState} from 'react';
+
+//************************* Form to add a new plant *************************
+
+const NewPlant = props => {
+    // const [input, setInput] = useState({ name: "", email: "", role: "", id: null });
+    const [input, setInput] = useState({ name: "", type: "", location: "", id: null });
+
+//************************* Form Handlers *************************
+
+const inputHandler = event => {
+    setInput({ ...input, [event.target.name]: event.target.value });
+};
+
+const submitHandler = event => {
+    event.preventDefault();
+    props.add({ ...input, id: Math.random() });
+    setInput({ name: "", type: "", location: "", id: null });
+};
+
+// *************************
+
+  return (
+    <form onSubmit={submitHandler} className="form">
+        <h1>Add Plant</h1>
+
+        <div className="input-container">
+            {/* Name of plant input */}
+            <label htmlFor="Name">
+            Name:{" "}
+            <input type="text" value={input.name} onChange={inputHandler} name="name" />
+            </label>
+            
+            {/* Type of plant input */}
+            <label htmlFor="Type">
+            Type:{" "}
+            <input type="text" value={input.type} onChange={inputHandler} name="type" />
+            </label>
+
+            {/* Location of plant */}
+            <label htmlFor="Location">
+            Location:{" "}
+            <input type="text" value={input.location} onChange={inputHandler} name="location" />
+            </label>
+        </div>
+
+        <button>Add Plant</button>
+    </form>
+  );
+};
+
+export default NewPlant;
