10b2b575a826e8ed35fa0b4e38aea89d1940fa7e
Hannah Tuttle <hannah@Hannahs-Air.PK5001Z>
2019-07-30 16:12:16 -0700

    button color is changing one button when any button is clicked, working fix

--- water_my_plants/src/components/Weekdays.js
+++ water_my_plants/src/components/Weekdays.js
@@ -1,5 +1,8 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 import styled from "styled-components"
+
+import {useWeekdayClick} from '../hooks/useWeekdayClick.js'
+
 
 const StyledWeekdays = styled.div`
     display: flex;
@@ -13,9 +16,10 @@ const StyledWeekdayDiv = styled.div`
 
 const StyledWeekdayButton= styled.button`
     border-radius: 50%;
+    outline: none;
 `
 
-export const Weekdays = () => {
+export const Weekdays = (props) => {
 
     const daysOfTheWeek= [{
         'M': 'monday'}, 
@@ -28,23 +32,35 @@ export const Weekdays = () => {
 
     const [weekdays, setWeekdays] = useState(daysOfTheWeek)
 
-    function Clicked(event) {
-        console.log('clicked a day', weekdays)
-        setWeekdays(weekdays)
-    }
+    // function Clicked(event) {
+        
+    //     setWeekdays(weekdays)
+    // }
+
+    const [clickedWeekday, setClickedWeekday] = useWeekdayClick(false);
+    const Clicked = e => {
+        e.preventDefault();
+        console.log('clicked a day',)
+        setClickedWeekday(!clickedWeekday);
+    };
+
+    // useEffect(() => {
+    //     const day = document.querySelector('button')
+    //     day.classList.add('clicked')
+    // },[weekdays])
 
     return (
         <StyledWeekdays>
             <h2>Weekdays to water</h2>
             <div></div>
         <StyledWeekdayDiv>
-            <StyledWeekdayButton name='Su' onClick={Clicked}>S</StyledWeekdayButton>
-            <StyledWeekdayButton >M</StyledWeekdayButton>
-            <StyledWeekdayButton >T</StyledWeekdayButton>
-            <StyledWeekdayButton >W</StyledWeekdayButton>
-            <StyledWeekdayButton>T</StyledWeekdayButton>
-            <StyledWeekdayButton>F</StyledWeekdayButton>
-            <StyledWeekdayButton>S</StyledWeekdayButton>
+            <StyledWeekdayButton onClick={Clicked}>S</StyledWeekdayButton>
+            <StyledWeekdayButton onClick={Clicked}>M</StyledWeekdayButton>
+            <StyledWeekdayButton onClick={Clicked}>T</StyledWeekdayButton>
+            <StyledWeekdayButton onClick={Clicked}>W</StyledWeekdayButton>
+            <StyledWeekdayButton onClick={Clicked}>T</StyledWeekdayButton>
+            <StyledWeekdayButton onClick={Clicked}>F</StyledWeekdayButton>
+            <StyledWeekdayButton onClick={Clicked}>S</StyledWeekdayButton>
         </StyledWeekdayDiv>
         </StyledWeekdays>
     )
---
+++ water_my_plants/src/hooks/useWeekdayClick.js
@@ -0,0 +1,22 @@
+import React, { useEffect } from 'react'
+
+import useLocalStorage from './useLocalStorage.js'
+
+export const useWeekdayClick =() => {
+
+    const [someValue, setSomeValue] =useLocalStorage('')
+
+    useEffect(() => {
+        const day = document.querySelector('button')
+        if(someValue === true){
+            day.classList.add('clicked')
+        }else{
+            day.classList.remove('clicked')
+        }
+    }, [someValue])
+
+    return [someValue, setSomeValue]
+}
+
+// export default useWeekdayClick
+
