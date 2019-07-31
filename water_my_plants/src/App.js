a23a81053a5a619823db92ceda6a6b6e98a5ccea
Reed James <reedtjames1234@gmail.com>
2019-07-31 07:56:55 -0500

    Commit to pull new master

--- water_my_plants/src/App.js
+++ water_my_plants/src/App.js
@@ -4,7 +4,7 @@ import { Route } from "react-router-dom";
 import "./App.css";
 
 import Login from "./components/login";
-import Register from "./components/Register";
+import Register from "./components/register";
 import { PrivateRoute } from "./components/PrivateRoute";
 import { PlantList } from "./components/PlantList";
 import { useToken } from "./hooks/useToken";
