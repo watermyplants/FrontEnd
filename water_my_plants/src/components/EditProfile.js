59409a4e36c63086389cdd3b087d9e35f7bde276
nisaChampagne <50988313+nisaChampagne@users.noreply.github.com>
2019-08-01 12:05:09 -0400

    edit btn

--- water_my_plants/src/components/EditProfile.js
+++ water_my_plants/src/components/EditProfile.js
@@ -2,11 +2,20 @@ import React, { useState } from 'react';
 import img from './photos/testimonial1.jpg'
 import styled from 'styled-components';
 
+
+
 const EditProfile = () => {
 //  const [editing, setEditing ] = useState(false);
 //  const initialFormState = { name:'', number: ''}
 //  const [account, setAccount ] = useState(initialFormState)
 
+
+const StyledEditBtn = styled.button`
+border: none;
+outline: none;
+background: white;
+color: teal;
+`;
 
     return(
         <div className='editprofile-container'>
@@ -19,9 +28,9 @@ const EditProfile = () => {
             <div className='middle-content'>
                 <div className='edit-info'>
                     <h4>Leah Potter</h4>
-                    <button>Edit name</button>
+                    <StyledEditBtn>Edit name</StyledEditBtn>
                     <p>+1(757) 932-3498</p>
-                    <button>Edit mobile number</button>
+                    <StyledEditBtn>Edit mobile number</StyledEditBtn>
                 </div>
                 <div className='bottom-content'>
                     <button>Cancel</button>
